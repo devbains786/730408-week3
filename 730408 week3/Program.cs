80b4f43d53c2104d769f6a67519a91bc5468e6b5
visualstudio <visualstudio@230-PC43>
2019-01-24 17:01:58 -0500

    Commit done 2019W CSD 3354 1

---
+++ 730408 week3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _730408_week3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
